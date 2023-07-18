a88c2aab5de067c444dc9236d662ec44fcebb680
Colin Smith <7762103+colinmxs@users.noreply.github.com>
2023-07-17 20:28:55 -0600

    run cdk init

---
+++ GithubIdp/src/GithubIdp/GithubIdpStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace GithubIdp
+{
+    public class GithubIdpStack : Stack
+    {
+        internal GithubIdpStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
